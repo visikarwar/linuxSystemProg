ce5fe8004b964784e32df22651dd2df5c0e7e8b8
Vikram Sikarwar <viks1329@gmail.com>
2021-06-11 23:41:46 +0530

    changes

---
+++ t_sysconf.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> 
+#include <errno.h>
+
+static void sysconfPrint(const char* msg, int name)
+{
+    long lim;
+    
+    errno = 0;
+    lim = sysconf(name);
+    if(lim != -1){
+        printf("%s %ld\n", msg, lim);
+    }else{
+        if(errno == 0){
+            printf("%s (indeterminated)\n", msg);
+        }else{
+            printf("sysconf %s", msg);
+        }
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    sysconfPrint("_SC_ARG_MAX:          ", _SC_ARG_MAX);
+    sysconfPrint("_SC_LOGIN_NAME_MAX:   ", _SC_LOGIN_NAME_MAX);
+    sysconfPrint("_SC_OPEN_MAX:         ", _SC_OPEN_MAX);
+    sysconfPrint("_SC_NGROUPS_MAX:      ", _SC_NGROUPS_MAX);
+    sysconfPrint("_SC_PAGESIZE:         ", _SC_PAGESIZE);
+    sysconfPrint("_SC_RTSIG_MAX:        ", _SC_RTSIG_MAX);
+    exit(0);
+}
